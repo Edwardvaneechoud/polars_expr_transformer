46eed12f69691a910381536daea9cc0701cb4c04
edwardvaneechoud <evaneechoud@gmail.com>
2024-06-11 18:08:56 +0200

    import error

--- polars_expr_transformer/__init__.py
+++ polars_expr_transformer/__init__.py
@@ -1,2 +1,1 @@
-from polars_expr_transformer.main_module import build_func, simple_function_to_expr
-from frame import LazyFrame, DataFrame
+from polars_expr_transformer.main_module import build_func, simple_function_to_expr, DataFrame, LazyFrame
--- polars_expr_transformer/main_module.py
+++ polars_expr_transformer/main_module.py
@@ -1,2 +1,2 @@
 from polars_expr_transformer.process.polars_expr_transformer import simple_function_to_expr, build_func
-from frame import LazyFrame, DataFrame
+from polars_expr_transformer.frame import LazyFrame, DataFrame
