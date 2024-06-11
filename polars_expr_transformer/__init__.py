859ec807d487a738dd4aa79acacd6c7ea535d50b
edwardvaneechoud <evaneechoud@gmail.com>
2024-06-11 18:06:59 +0200

    adding dataframe and lazyframe implementation

--- polars_expr_transformer/__init__.py
+++ polars_expr_transformer/__init__.py
@@ -1,1 +1,2 @@
 from polars_expr_transformer.main_module import build_func, simple_function_to_expr
+from frame import LazyFrame, DataFrame
--- polars_expr_transformer/main_module.py
+++ polars_expr_transformer/main_module.py
@@ -1,1 +1,2 @@
 from polars_expr_transformer.process.polars_expr_transformer import simple_function_to_expr, build_func
+from frame import LazyFrame, DataFrame
