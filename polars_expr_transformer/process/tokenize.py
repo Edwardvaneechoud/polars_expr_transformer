746e4222c2e9bf05caaad9e08da507824ae2c71b
Edward van Eechoud <41021650+Edwardvaneechoud@users.noreply.github.com>
2025-03-18 21:23:49 +0100

    Adding demo version and fixing issues with and operators (#119)
    * Adding demo version and fixing issues with and operators
    
    * removing the lock
    
    * fix version for streamlit

--- polars_expr_transformer/process/tokenize.py
+++ polars_expr_transformer/process/tokenize.py
@@ -11,6 +11,7 @@ def tokenize(formula: str):
     Returns:
         A list of tokens extracted from the formula string.
     """
+
     r = list(formula[::-1])
     output = []
     v = ''
@@ -18,68 +19,119 @@ def tokenize(formula: str):
     in_brackets = False  # Flag to track if we're inside square brackets
     i = 0
     string_indicator = None
+
     while i < len(r):
         current_val = r[i]
-        # print(i, v[::-1])
+
         if current_val == string_indicator:
-            output.append(v+current_val)
+            v += current_val  # Add the closing quote
+            output.append(v)  # Add the complete string literal to output
             v = ''
             string_indicator = None
             in_string = False
             i += 1
             continue
         elif current_val in ('"', "'") and string_indicator is None:
-            in_string = True  # Toggle the in_string flag
+            # We're starting a new string literal
+            if v:  # If we have accumulated any non-string content
+                output.append(v)
+                v = ''
+            in_string = True
             string_indicator = current_val
+            v = current_val  # Start the string with the opening quote
+            i += 1
+            continue
+
+        # If we're inside a string literal, just accumulate characters
+        if in_string:
+            v += current_val
+            i += 1
+            continue
+
+        # Handle brackets
         elif current_val in ['[', ']']:
             in_brackets = not in_brackets  # Toggle the in_brackets flag
-        elif current_val == '=' and not in_brackets and not in_string:
+
+        # Handle equality operators
+        elif current_val == '=' and not in_brackets:
             if len(r) > i + 1:
                 two_character_inline = r[i + 1] in ('<', '>', '=', '!')
                 if two_character_inline:
                     current_val += r[i + 1]
                     i += 1
+
+        # Check for logical operators, but ONLY outside of strings
+        if not in_string and not in_brackets:
+            # Check for ' and ' (reversed)
+            if i + 4 < len(r) and r[i:i + 5] == list(' dna '):
+                if v:
+                    output.append(v)
+                output.append('dna')
+                v = ''
+                i += 5
+                continue
+
+            # Check for ' or ' (reversed)
+            if i + 3 < len(r) and r[i:i + 4] == list(' ro '):
+                if v:
+                    output.append(v)
+                output.append('ro')
+                v = ''
+                i += 4
+                continue
+
+        # Handle normal split values (only outside strings)
         if not in_string and not in_brackets and current_val[::-1] in all_split_vals:
-            if i > 0:
+            if v:
                 output.append(v)
             output.append(current_val)
             v = ''
-        elif any([vv[::-1] in v+current_val for vv in all_split_vals if len(vv) > 1]) and not in_string:
-            splitter = next(vv[::-1] for vv in all_split_vals if len(vv) > 1 and vv[::-1] in v+current_val)
+        elif not in_string and any([vv[::-1] in v + current_val for vv in all_split_vals if len(vv) > 1]):
+            splitter = next((vv[::-1] for vv in all_split_vals if len(vv) > 1 and vv[::-1] in v + current_val), None)
             if splitter:
-                # check for longer possiblities
-                longer_options = [f for f in all_functions.keys() if (v+current_val)[::-1] in f]
-                if len(longer_options)>0:
+                # check for longer possibilities
+                longer_options = [f for f in all_functions.keys() if (v + current_val)[::-1] in f]
+                if len(longer_options) > 0:
                     temp_i, temp_v = i, v
-                    while temp_i<len(r) and len([f for f in all_functions.keys() if (temp_v+r[temp_i])[::-1] in f])>0:
+                    while temp_i < len(r) and len(
+                            [f for f in all_functions.keys() if (temp_v + r[temp_i])[::-1] in f]) > 0:
                         temp_v += r[temp_i]
                         temp_i += 1
 
-                    other_split = next((f for f in all_functions.keys() if temp_v[::-1] == f))
-                    next_value = r[temp_i] if temp_i<len(r) else None
-                    if next_value in [None, ' '] + list(set(v[0] for v in all_split_vals if len(v)>0)) and other_split is not None:
+                    other_split = next((f for f in all_functions.keys() if temp_v[::-1] == f), None)
+                    next_value = r[temp_i] if temp_i < len(r) else None
+                    if next_value in [None, ' '] + list(
+                            set(v[0] for v in all_split_vals if len(v) > 0)) and other_split is not None:
                         output.append(temp_v)
                         v = ''
                         i = temp_i
                         continue
-            for toks in (v+current_val).split(splitter):
-                if len(toks) > 0:
-                    output.append(toks)
-            output.append(splitter)
-            v = ''
+
+                for toks in (v + current_val).split(splitter):
+                    if len(toks) > 0:
+                        output.append(toks)
+                output.append(splitter)
+                v = ''
+            else:
+                v += current_val
         else:
             v += current_val
         i += 1
 
-    if v is not None and any([vv[::-1] in v for vv in all_split_vals if len(vv) > 1]):
-        splitter = next(vv[::-1] for vv in all_split_vals if len(vv) > 1 and vv[::-1] in v)
+    # Process any remaining content
+    if v:
+        if not in_string and any([vv[::-1] in v for vv in all_split_vals if len(vv) > 1]):
+            splitter = next((vv[::-1] for vv in all_split_vals if len(vv) > 1 and vv[::-1] in v), None)
+            if splitter:
+                for toks in v.split(splitter):
+                    if len(toks) > 0:
+                        output.append(toks)
+                output.append(splitter)
+        else:
+            output.append(v)
 
-        for toks in v.split(splitter):
-            if len(toks) > 0:
-                output.append(toks)
-        output.append(splitter)
-    elif v is not None:
-        output.append(v)
+    # Reverse the characters in each token and reverse the order of tokens
     output = [''.join(reversed(v)) for v in output]
     output.reverse()
+
     return output
